16b473363ccb18b3de6b21451c34b675e35459a0
Howl Anderson <u1mail2me@gmail.com>
2014-10-13 09:39:58 +0800

    do some improve

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install libgdal-dev python-setuptools libpython-dev python-pip python-mysqldb
+sudo apt-get install libgdal-dev python-setuptools libpython-dev python-pip python-mysqldb python-gdal
 
-sudo pip install tablib flask flask-login sqlalchemy
+sudo pip install tablib flask flask-login sqlalchemy iso3166
